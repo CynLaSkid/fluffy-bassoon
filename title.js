14c2ba0934fcaa23e128813553e03497244d2200
bubatz <71530056+cynhook@users.noreply.github.com>
2022-03-10 16:17:52 -0500

    Add files via upload

---
+++ title.js
@@ -0,0 +1,3 @@
+var i=0;setInterval(function(){var titles=["| d |","| de |","| des |","| desi |","| desir |","| desire |","| desired |","| desire |","| desir |","| desi |","| des |","| de |","| d |"]
+if(i===titles.length){i=0;}
+document.title=titles[i];i++;},900);
